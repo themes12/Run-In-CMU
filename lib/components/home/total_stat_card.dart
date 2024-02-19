7723172617a5560659f240f6771490f1448d5115
faii with an i <pattamatha12@gmail.com>
2024-02-19 22:08:02 +0700

    total stat card

---
+++ lib/components/home/total_stat_card.dart
@@ -0,0 +1,181 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:runinmor/types/route_list.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class TotalStatsCard extends StatelessWidget {
+  final String distance;
+  final String pace;
+  final String time;
+
+  const TotalStatsCard({
+    Key? key,
+    required this.distance,
+    required this.pace,
+    required this.time,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGrid.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      children: [
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 1.4,
+          child: Card(
+            elevation: 0,
+            margin: EdgeInsets.zero,
+            color: Color(0xFF6B5886),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    FluentIcons.run_24_regular,
+                    color: Color(0xFFFFFFFF),
+                    size: 40,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '$distance',
+                    style: TextStyle(
+                      fontSize: 48,
+                      color: Color(0xFFFFFFFF),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Kilometres',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xFFEFEFEF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 0.7,
+          child: Card(
+            elevation: 0,
+            margin: EdgeInsets.zero,
+            color: Color(0xFFB49AD9),
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FluentIcons.notebook_20_regular,
+                        color: Color(0xFFFFFFFF),
+                        size: 40,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '$pace',
+                        style: TextStyle(
+                        fontSize: 24,
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.bold,),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Avg. Pace',
+                        style: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFFEFEFEF),),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        StaggeredGridTile.count(
+          crossAxisCellCount: 1,
+          mainAxisCellCount: 0.7,
+
+          child: Card(
+            elevation: 0,
+            margin: EdgeInsets.zero,
+            color: Color(0xFFC6B4DF),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FluentIcons.timer_24_regular,
+                          color: Color(0xFFFFFFFF),
+                          size: 40,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          '$time',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Color(0xFFFFFFFF),
+                            fontWeight: FontWeight.bold,),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          'Time',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Color(0xFFEFEFEF),),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+            ),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,12 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../components/home/home_stat_card.dart';
+import '../components/home/total_stat_card.dart';
 import '../components/template/white_container.dart';
 import '../provider/navigation_bar_provider.dart';
-import 'home_stat_card.dart'; // Import the BestStatsCard widget
+// Import the BestStatsCard widget
 
 
 class HomePage extends StatelessWidget {
@@ -14,79 +14,109 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final navigation =
-        Provider.of<NavigationBarProvider>(context, listen: false);
+    Provider.of<NavigationBarProvider>(context, listen: false);
 
     return Scaffold(
       backgroundColor: const Color(0xFF714DA5),
       body: WhiteContainer(
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Ready to run?',
-              style: TextStyle(
-                color: Color(0xFF714DA5),
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Ready to run?',
+                style: TextStyle(
+                  color: Color(0xFF714DA5),
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 6),
-              child: Row(
-                children: [
-                  Icon(
-                    FluentIcons.calendar_28_regular,
-                    color: Color(0xFF7A7A7A),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '9 February 2024',
-                    style: TextStyle(
+              Padding(
+                padding: EdgeInsets.only(top: 6),
+                child: Row(
+                  children: [
+                    Icon(
+                      FluentIcons.calendar_28_regular,
                       color: Color(0xFF7A7A7A),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
                     ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 36),
-              child: Column(
-                children: [
-                  Text(
-                    'Best stats',
-                    style: TextStyle(
-                      color: Color(0xFF714DA5),
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
+                    SizedBox(
+                      width: 10,
                     ),
-                  ),
-                  // Add the BestStatsCard widget here
-
-                ],
+                    Text(
+                      '9 February 2024',
+                      style: TextStyle(
+                        color: Color(0xFF7A7A7A),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: Column(
-                children: [
-                  // Add the BestStatsCard widget here
-                  const BestStatsCard(
-                    name: 'Aung Keaw',
-                    distance: '2.64',
-                    pace: '11\'59"',
-                    time: '31:04',
-                    imageUrl: '',
-                  ),
+              Padding(
+                padding: EdgeInsets.only(top: 36),
+                child: Column(
+                  children: [
+                    Text(
+                      'Best stats',
+                      style: TextStyle(
+                        color: Color(0xFF714DA5),
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    // Add the BestStatsCard widget here
 
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  children: [
+                    // Add the BestStatsCard widget here
+                    const BestStatsCard(
+                      name: 'Aung Keaw',
+                      distance: '2.64',
+                      pace: '11\'59"',
+                      time: '31:04',
+                      imageUrl: '',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 36),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total stats',
+                      style: TextStyle(
+                        color: Color(0xFF714DA5),
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    // Add the BestStatsCard widget here
+
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  children: [
+                    // Add the BestStatsCard widget here
+                    const TotalStatsCard(
+                      distance: '2.64',
+                      pace: '11\'59"',
+                      time: '31:04',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         // child: Row(
         //   crossAxisAlignment: CrossAxisAlignment.start,
